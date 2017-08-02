6fe211ff2df9ece1949c5e04a75a36284e03669d
Alex Burka <aburka@seas.upenn.edu>
2017-08-02 12:07:13 -0400

    don't neglect directory sizes

--- posts/du-evolution/code/du/src/bin/v1.rs
+++ posts/du-evolution/code/du/src/bin/v1.rs
@@ -7,37 +7,38 @@ extern crate du;
 use du::errors::*;
 
 fn local_du(path: &Path) -> Result<u64> {
-  Ok(path.read_dir().chain_err(|| Io("list", path.into()))?
-         .map(|res| {
-             match res {
-                 Ok(entry) => {
-                     match entry.metadata() {
-                         Ok(meta) => {
-                             if meta.is_dir() {
-                                 local_du(&entry.path()).unwrap_or_else(|e| {
-                                     println!("warning: could not list directory {} (skipping): {}",
-                                     entry.path().display(), e);
-                                     0
-                                 })
-                             } else {
-                                 meta.len()
-                             }
-                         }
-                         Err(e) => {
-                             println!("warning: could not stat {} (skipping): {}",
-                                      entry.path().display(), e);
-                             0
-                         }
-                     }
-                 }
-                 Err(e) => {
-                     println!("warning: could not stat entry in {} (skipping): {}",
-                              path.display(), e);
-                     0
-                 }
-             }
-         })
-         .fold(0, |a, b| a+b))
+    Ok(path.read_dir().chain_err(|| Io("list", path.into()))?
+       .map(|res| {
+           match res {
+               Ok(entry) => {
+                   match entry.metadata() {
+                       Ok(meta) => {
+                           if meta.is_dir() {
+                               local_du(&entry.path()).unwrap_or_else(|e| {
+                                   println!("warning: could not list directory {} (skipping): {}",
+                                   entry.path().display(), e);
+                                   0
+                               })
+                           } else {
+                               meta.len()
+                           }
+                       }
+                       Err(e) => {
+                           println!("warning: could not stat {} (skipping): {}",
+                           entry.path().display(), e);
+                           0
+                       }
+                   }
+               }
+               Err(e) => {
+                   println!("warning: could not stat entry in {} (skipping): {}",
+                   path.display(), e);
+                   0
+               }
+           }
+       })
+    .fold(path.metadata().chain_err(|| Io("stat", path.into()))?.len(),
+          |a, b| a+b))
 }
 
 fn main() {
