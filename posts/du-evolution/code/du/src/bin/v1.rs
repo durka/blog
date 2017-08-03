2daadcea293b5e4f5e109c3ad1d11d045cbd3706
Alex Burka <aburka@seas.upenn.edu>
2017-08-02 23:11:04 -0400

    dump rustfmt

--- posts/du-evolution/code/du/src/bin/v1.rs
+++ posts/du-evolution/code/du/src/bin/v1.rs
@@ -10,35 +10,32 @@ fn local_du(path: &Path) -> Result<u64> {
     Ok(path.read_dir()
            .chain_err(|| Io("list", path.into()))?
            .map(|res| match res {
-                    Ok(entry) => {
-                        match entry.metadata() {
-                            Ok(meta) => {
-                                if meta.is_dir() {
-                                    local_du(&entry.path()).unwrap_or_else(|e| {
-                    println!("warning: could not list directory {} (skipping): {}",
-                             entry.path().display(),
-                             e);
-                    0
-                })
-                                } else {
-                                    meta.len()
-                                }
-                            }
-                            Err(e) => {
-                                println!("warning: could not stat {} (skipping): {}",
-                                         entry.path().display(),
-                                         e);
-                                0
-                            }
-                        }
-                    }
-                    Err(e) => {
-                        println!("warning: could not stat entry in {} (skipping): {}",
-                                 path.display(),
-                                 e);
-                        0
-                    }
-                })
+               Ok(entry) => {
+                   match entry.metadata() {
+                       Ok(meta) => {
+                           if meta.is_dir() {
+                               local_du(&entry.path()).unwrap_or_else(|e| {
+                                   println!("warning: could not list directory {} (skipping): {}",
+                                            entry.path().display(), e);
+                                   0
+                               })
+                           } else {
+                               meta.len()
+                           }
+                       }
+                       Err(e) => {
+                           println!("warning: could not stat {} (skipping): {}",
+                                    entry.path().display(), e);
+                           0
+                       }
+                   }
+               }
+               Err(e) => {
+                   println!("warning: could not stat entry in {} (skipping): {}",
+                            path.display(), e);
+                   0
+               }
+           })
            .fold(path.metadata().chain_err(|| Io("stat", path.into()))?.len(),
                  |a, b| a + b))
 }
