31bf65dbc6feee31c3ad50ef7cd848c6e421f936
Alex Burka <aburka@seas.upenn.edu>
2017-08-05 11:14:57 -0400

    also measure native du

--- _posts/du-evolution/code/du/bench.sh
+++ _posts/du-evolution/code/du/bench.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-NWARM=10
-NITER=50
-DIR=/Users/alex/.cargo
+NWARM=20
+NITER=100
+DIR=$HOME/.cargo
 
+function go() {
+    echo -n "Warming up $2..."
+    for i in $(seq $NWARM); do
+        echo -n " $i"
+        $1 $DIR >/dev/null 2>&1
+    done
+    echo ""
+    echo -n "Timing $2..."
+    echo >$2.time
+    for j in $(seq $NITER); do
+        echo -n " $j"
+        /usr/bin/time -p $1 $DIR 2>&1| rg real | awk '{print $2}' >>$2.time
+    done
+    echo ""
+}
+
+go "/usr/bin/du -s" du
 for n in 1 2 3; do
     echo "Compiling v$n..."
     cargo build --release --bin v$n
-    echo -n "Warming up v$n..."
-    for i in $(seq $NWARM); do
-        target/release/v$n $DIR >/dev/null 2>&1
-    done
-    echo ""
-    echo -n "Timing v$n..."
-    for j in $(seq $NITER); do
-        /usr/bin/time -p target/release/v$n $DIR 2>&1| rg real | awk '{print $2}'
-    done | st 2>/dev/null | rg 'mean|stddev'
-    echo ""
+    go target/release/v$n v$n
 done
 
