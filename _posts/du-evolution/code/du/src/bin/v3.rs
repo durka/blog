9955107fc41a44f9b7bcc08c083a83dee121b714
Alex Burka <aburka@seas.upenn.edu>
2017-08-05 11:12:59 -0400

    remove debugging code

--- _posts/du-evolution/code/du/src/bin/v3.rs
+++ _posts/du-evolution/code/du/src/bin/v3.rs
@@ -14,9 +14,8 @@ error_chain! {
 }
 
 fn local_du<P: AsRef<Path>>(path: P) -> Result<u64> {
-    use std::io::Write; let mut f = std::fs::File::create("v3.log").unwrap();
     Ok(convert(WalkDir::new(path).into_iter())
-           .and_then(|entry| { writeln!(&mut f, "{} {}", entry.metadata().unwrap().len(), entry.path().display()).unwrap(); Ok(entry.metadata()?.len()) })
+           .and_then(|entry| Ok(entry.metadata()?.len()))
            .fold(0, |a, b| a + b)?)
 }
 
